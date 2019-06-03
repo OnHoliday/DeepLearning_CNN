157e930c1c6112983be01f98a1b459c6bae56593
Joana <joana.lorenz@outlook.de>
2019-06-03 11:55:24 +0100

    Merge branch 'master' of C:\Users\jojo\Documents\Uni\Second Semester\Deep Learning\Project\Master with conflicts.

--- MULTI_CNN.py
+++ MULTI_CNN.py
@@ -1,120 +1,82 @@
 
 # import the necessary packages
-from keras.engine.saving import save_model, load_model
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Dropout
-from keras.optimizers import Adam
 from keras.layers.core import Lambda
 from keras.layers.core import Dense
 from keras.layers import Flatten
 from keras.layers import Input
 import tensorflow as tf
 
-from utils import *
+# source: https://www.pyimagesearch.com/2018/06/04/keras-multiple-outputs-and-multiple-losses/?fbclid=IwAR1CTwo64m-YJW4L6L2ab9v2Jm0xajlFrdCj16HF8N7pILWp8WKQLB9WgZI
 
 class Group12Net:
 
-    def __init__(self, model_name):
-        self.model_name = model_name
-
-    def build_gender_branch(self, inputs, numGender, finalAct="softmax", chanDim=-1):
+    def build_gender_branch(inputs, numGender, finalAct="softmax", chanDim=-1):
 
         x = inputs
 
         # CONV => RELU => POOL
         x = Conv2D(32, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=chanDim)(x)
         x = MaxPooling2D(pool_size=(3, 3))(x)
-        # x = Dropout(0.25)(x)
+        x = Dropout(0.25)(x)
 
         # (CONV => RELU) * 2 => POOL
-        # x = Conv2D(64, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
-        # x = Conv2D(64, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
-        # x = MaxPooling2D(pool_size=(2, 2))(x)
-        # x = Dropout(0.25)(x)
+        x = Conv2D(64, (3, 3), padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=chanDim)(x)
+        x = Conv2D(64, (3, 3), padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=chanDim)(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.25)(x)
 
-        # define a branch of output layers for the number of different
-        # clothing categories (i.e., shirts, jeans, dresses, etc.)
         x = Flatten()(x)
         x = Dense(256)(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.35)(x)
         x = Dense(numGender)(x)
         x = Activation(finalAct, name="gender_output")(x)
 
         # return the category prediction sub-network
         return x
 
-    def build_race_branch(self, inputs, numRace, finalAct="softmax", chanDim=-1):
+    def build_race_branch(inputs, numRace, finalAct="softmax", chanDim=-1):
 
         x = inputs
         # CONV => RELU => POOL
-        # x = Conv2D(16, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
-        # x = MaxPooling2D(pool_size=(3, 3))(x)
-        # x = Dropout(0.25)(x)
+        x = Conv2D(16, (3, 3), padding="same")(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=chanDim)(x)
+        x = MaxPooling2D(pool_size=(3, 3))(x)
+        x = Dropout(0.25)(x)
 
         # CONV => RELU => POOL
         x = Conv2D(32, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization(axis=chanDim)(x)
         x = MaxPooling2D(pool_size=(2, 2))(x)
-        # x = Dropout(0.25)(x)
+        x = Dropout(0.25)(x)
 
-        # define a branch of output layers for the number of different
-        # colors (i.e., red, black, blue, etc.)
         x = Flatten()(x)
         x = Dense(128)(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization()(x)
-        # x = Dropout(0.35)(x)
+        x = Activation("relu")(x)
+        x = BatchNormalization()(x)
+        x = Dropout(0.35)(x)
         x = Dense(numRace)(x)
         x = Activation(finalAct, name="race_output")(x)
 
         # return the color prediction sub-network
         return x
 
-    def build_age_branch(self, inputs, chanDim=-1):
-
-        x = inputs
-        # CONV => RELU => POOL
-        x = Conv2D(16, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
-        x = MaxPooling2D(pool_size=(3, 3))(x)
-        # x = Dropout(0.25)(x)
-
-        # CONV => RELU => POOL
-        x = Conv2D(32, (3, 3), padding="same")(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization(axis=chanDim)(x)
-        x = MaxPooling2D(pool_size=(2, 2))(x)
-        # x = Dropout(0.25)(x)
-
-        # define a branch of output layers for the number of different
-        # colors (i.e., red, black, blue, etc.)
-        x = Flatten()(x)
-        x = Dense(128)(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization()(x)
-        x = Dense(64)(x)
-        # x = Activation("relu")(x)
-        # x = BatchNormalization()(x)
-        x = Dense(1, name="age_output", kernel_initializer='normal', activation='linear')(x)
-        return x
-
-
-    def build(self, width, height, numGender, numRace, finalAct="softmax"):
+    def build(width, height, numGender, numRace, finalAct="softmax"):
 
         # initialize the input shape and channel dimension (this code
         # assumes you are using TensorFlow which utilizes channels
@@ -122,117 +84,18 @@ class Group12Net:
         inputShape = (height, width, 3)
         chanDim = -1
 
-        # construct both the "category" and "color" sub-networks
+        # construct both sub-networks
         inputs = Input(shape=inputShape)
-
-        gender_branch = self.build_gender_branch(inputs, numGender, finalAct=finalAct, chanDim=chanDim)
-        race_branch = self.build_race_branch(inputs, numRace, finalAct=finalAct, chanDim=chanDim)
-        age_branch = self.build_age_branch(inputs, chanDim=chanDim)
+        gender_branch = Group12Net.build_gender_branch(inputs, numGender, finalAct=finalAct, chanDim=chanDim)
+        race_branch = Group12Net.build_race_branch(inputs, numRace, finalAct=finalAct, chanDim=chanDim)
 
         # create the model using our input (the batch of images) and
-        # two separate outputs -- one for the clothing category
-        # branch and another for the color branch, respectively
-        self.model = Model(
+        # two separate outputs for the gender and the race branch
+        model = Model(
             inputs=inputs,
-            outputs=[gender_branch, race_branch, age_branch],
-            name=self.model_name)
-
-        self._compile_model()
-
-        # initialize the optimizer and compile the model
-        # print("[INFO] compiling model...")
-        print(self.model.summary())
-        # print("[INFO] Model compiled ...")
+            outputs=[gender_branch, race_branch],
+            name="Group12Net")
 
         # return the constructed network architecture
-        self.model
-
-    def load_model(self):
-        self.model = load_model(self.model_name)
-        self._compile_model()
-        print(self.model.summary())
-
-
-    def _save_model(self):
-        save_model(self.model, self.model_name)
-
-    def _compile_model(self):
-        EPOCHS = 50
-        INIT_LR = 1e-3
-        BS = 32
-        IMAGE_DIMS = (64, 64, 3)
-        losses = {
-            "gender_output": "binary_crossentropy",
-            "race_output": "categorical_crossentropy",
-            "age_output": "mean_absolute_error",
-        }
-        lossWeights = {"gender_output": 1.0, "race_output": 1.0, "age_output": 1.0}
-        opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-
-        self.model.compile(optimizer=opt, loss=losses, loss_weights=lossWeights, metrics={"gender_output":"accuracy", "race_output":"accuracy","age_output":"mae"})
-
-
-def built_multi(n_races,im_width):
-    from keras.layers import Input, Dense, BatchNormalization, Conv2D, MaxPool2D, GlobalMaxPool2D, Dropout
-    from keras.optimizers import SGD
-    from keras.models import Model
-
-    def conv_block(inp, filters=32, bn=True, pool=True):
-        _ = Conv2D(filters=filters, kernel_size=3, activation='relu', padding="same")(inp)
-        if bn:
-            _ = BatchNormalization()(_)
-        if pool:
-            _ = MaxPool2D()(_)
-        return _
-
-    input_layer = Input(shape=(im_width, im_width, 3))
-    _ = conv_block(input_layer, filters=32, bn=False, pool=False)
-    _ = conv_block(_, filters=32)
-    _ = conv_block(_, filters=32*2)
-    _ = conv_block(_, filters=32*4)
-    _ = conv_block(_, filters=32*5)
-    _ = conv_block(_, filters=32*6)
-    bottleneck = GlobalMaxPool2D()(_)#bottleneck???
-
-    # for age calculation
-    _ = Dense(units=256, activation='relu')(bottleneck)
-    _ = Dropout(0.3)(_)
-    _ = Dense(units=128, activation='relu')(_)
-    _ = Dropout(0.3)(_)
-    age_output = Dense(units=1, activation='sigmoid', name='age_output')(_)
-
-    # for race prediction
-    _ = Dense(units=256, activation='relu')(bottleneck)
-    _ = Dropout(0.3)(_)
-    _ = Dense(units=128, activation='relu')(_)
-    _ = Dropout(0.3)(_)
-    race_output = Dense(units=n_races, activation='softmax', name='race_output')(_)
-
-    # for gender prediction
-    _ = Dense(units=128, activation='relu')(bottleneck)
-    _ = Dropout(0.3)(_)
-    gender_output = Dense(units=1, activation='sigmoid', name='gender_output')(_)
-
-    model = Model(inputs=input_layer, outputs=[age_output, race_output, gender_output])
-    model.compile(optimizer='rmsprop',
-                  loss={'age_output': 'mse', 'race_output': 'categorical_crossentropy',
-                        'gender_output': 'binary_crossentropy'},
-                  loss_weights={'age_output': 2., 'race_output': 1.5, 'gender_output': 1.},
-                  metrics={'age_output': 'mae', 'race_output': 'accuracy', 'gender_output': 'accuracy'})
-
-    return model
-
-def load_model_(model_name):
-    model = load_model(model_name)
-    model.compile(optimizer='rmsprop',
-                  loss={'age_output': 'mse', 'race_output': 'categorical_crossentropy',
-                        'gender_output': 'categorical_crossentropy'},
-                  loss_weights={'age_output': 2., 'race_output': 1.5, 'gender_output': 1.},
-                  metrics={'age_output': 'mae', 'race_output': 'accuracy', 'gender_output': 'accuracy'})
-
-    print(model.summary())
-    return model
-
-
-
+        return model
 
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras.preprocessing import image
-import os, shutil
+import os
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
@@ -9,25 +9,6 @@ import time
 from keras.callbacks import CSVLogger, EarlyStopping, ModelCheckpoint, TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
 import random
-from keras.utils import to_categorical
-from PIL import Image
-from pathlib import Path, PureWindowsPath
-import getpass
-import os
-from sklearn.metrics import confusion_matrix, classification_report
-import glob
-
-
-if getpass.getuser() == 'Konrad':
-    project_dir = Path(PureWindowsPath('D:\\DeepLearningProject'))
-elif getpass.getuser() == 'fruechtnicht':
-    project_dir = Path('/Users/fruechtnicht/NOVA/M.Sc_Data_Science_and_Advanced_Analytics/Semester2/Deep_Learning/Project/project_dir')
-elif getpass.getuser() == 'dominika.leszko':
-    project_dir = Path(r'C:\Users\dominika.leszko\Desktop\NOVAIMS\SEMESTER2\Deep Learinng\PROJECT\git_repo')
-else:
-    raise ValueError('Check you own user name and add proper elif statement !!!')
-
-
 
 def get_current_directory():
     path = os.getcwd()
@@ -40,17 +21,10 @@ def get_time_stamp():
 
 
 def prepare_input_data(path, nr_of_examples):
-    lst = os.listdir(path)
-
-    for item in lst:
-        if not item.endswith(".jpg"):
-            os.remove(path / item)
 
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
-    random.shuffle(onlyfiles)
-
-    df = pd.DataFrame(columns=['age', 'gender', 'ethnic','file_name'])
+    df = pd.DataFrame(columns=['age', 'gender', 'ethnic', 'file_name'])
     index = 0
     for item in onlyfiles:
         if index < nr_of_examples:
@@ -59,42 +33,12 @@ def prepare_input_data(path, nr_of_examples):
                 age, gender, ethnic, time = a[0], a[1], a[2], a[3]
             except:
                 pass
-            data = time[:8]
+            # data = time[:8]
             df.loc[index, ['age', 'gender', 'ethnic', 'file_name']] = age, gender, ethnic, item
             index += 1
         else:
-            df['age'] = df['age'].astype('float')
-
             return df
-    df['age'] = df['age'].astype('float')
     return df
-
-def organize_cropped_files(project_dir):
-    #create train & test directories
-    train_dir = os.path.join(project_dir, 'UTKFace')
-    os.mkdir(project_dir / 'UTKFace_test')
-    test_dir = os.path.join(project_dir, 'UTKFace_test')
-    os.mkdir(project_dir / 'UTKFace_pred')
-    pred_dir=os.path.join(project_dir, 'UTKFace_pred')
-    #move random 20% files to UTKFace_test
-    onlyfiles = [f for f in listdir(project_dir / 'UTKFace') if isfile(join(project_dir / 'UTKFace', f))]
-    random.shuffle(onlyfiles)
-    t=int(0.8*len(onlyfiles))
-    mv_files=onlyfiles[t:]
-    for i in range(len(mv_files)):
-        i_path_from=os.path.join(train_dir, mv_files[i])
-        i_path_to=os.path.join(test_dir, mv_files[i])
-        shutil.move(i_path_from, i_path_to)
-    # move random 1% files to UTKFace_pred
-    random.shuffle(mv_files)
-    t2=int(0.99*len(mv_files))
-    mv_files2=mv_files[t2:]
-    for i in range(len(mv_files2)):
-        i_path_from2=os.path.join(test_dir, mv_files2[i])
-        i_path_to2=os.path.join(pred_dir, mv_files2[i])
-        shutil.move(i_path_from2, i_path_to2)
-
-
 
 
 def save_model(classifier, model_name):
@@ -113,10 +57,6 @@ def save_model(classifier, model_name):
 def load_model(model_name):
     # load json and create model
     model_name_json = model_name + '.json'
-# Uncoment to use Lucas CVS
-#    model_name_type = model_name + '.json'
-#    model_path = project_dir / 'lucas_log_models'/ 'cvs' / model_name_type
-#     json_file = open(model_path, 'r')
     json_file = open(model_name_json, 'r')
     loaded_model_json = json_file.read()
     json_file.close()
@@ -126,9 +66,7 @@ def load_model(model_name):
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
     model_name_h5 = model_name + '.h5'
-    model_path = project_dir / 'lucas_log_models' / 'cvs' / model_name_h5
-
-    loaded_model.load_weights(model_path)
+    loaded_model.load_weights(model_name_h5)
     print("Loaded model from disk")
     return loaded_model
 
@@ -143,7 +81,7 @@ def callback_history():
     return history
 
 def callbackEarlyStopping():
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4, min_delta=0.0001)
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2, min_delta=1)
     return es
 
 def callbackCheckpoint(model_name):
@@ -155,21 +93,14 @@ def callbackTensor():
     tb = TensorBoard(log_dir='/logs', histogram_freq=0, write_graph=True, write_images=True)
     return tb
 
-
-def create_unique_cv_logger():
+def create_cv_logger():
     wd = get_current_directory()
     now = get_time_stamp()
-    name = Path(now + '_training.csv')
-    name = project_dir / Path('log') / name
+    name = now + '_training.csv'
+    name = wd + r'/log/' + name
     csv_logger = CSVLogger(name)
     return csv_logger
 
-def create_cv_logger(model_name):
-    wd = get_current_directory()
-    name = model_name + '_training.csv'
-    name = wd + r'/log/' + name
-    csv_logger = CSVLogger(name, append=True)
-    return csv_logger
 
 def create_trainingDataGenerator_instance():
     train_datagen = ImageDataGenerator(rescale=1. / 255,
@@ -195,146 +126,21 @@ def create_set(datagen, df, path, target_size, batch_size, target, color_mode, c
     return set
 
 
-def gnerate_genarator_multi(datagen, df, path, target_size, batch_size, target1, target2, target3, color_mode, class_mode):
-    GENy1 = datagen.flow_from_dataframe(
-            dataframe = df,
-            directory = path,
-            x_col = 'file_name',
-            y_col = target1,
-            target_size = (target_size, target_size),
-            batch_size = batch_size,
-            color_mode = color_mode,
-            class_mode = class_mode,
-            seed = 1)
-
-    GENy2 = datagen.flow_from_dataframe(
-            dataframe = df,
-            directory = path,
-            x_col = 'file_name',
-            y_col = target2,
-            target_size = (target_size, target_size),
-            batch_size = batch_size,
-            color_mode = color_mode,
-            class_mode = class_mode,
-            seed = 1)
-
-
-    GENy3 = datagen.flow_from_dataframe(
-            dataframe = df,
-            directory = path,
-            x_col = 'file_name',
-            y_col = target3,
-            target_size = (target_size, target_size),
-            batch_size = batch_size,
-            color_mode = color_mode,
-            class_mode = 'other',
-            seed = 1)
-
-    while True:
-            y1 = GENy1.next()
-            y2 = GENy2.next()
-            y3 = GENy3.next()
-            yield y1[0], {'gender_output': y1[1], 'race_output': y2[1], 'age_output': y3[1]}
-
-
-
-def make_new_prediction(classifier, target, target_size, cropped=False):
+def make_new_prediction(classifier, target):
     wd = get_current_directory()
-    if cropped:
-        path1 = wd + '\\UTKFace_pred\\'#for cropped
-    else:
-        path1 = wd + '\part2\\'#for non-cropped
+    path1 = wd + '\part2\\'
 
     onlyfiles = [f for f in listdir(path1) if isfile(join(path1, f))]
-    random_pic = Path(random.choice(onlyfiles))
-    path = path1 / random_pic
+    random_pic = random.choice(onlyfiles)
+    path = wd + '\part2\\' + random_pic
 
-    test_image = image.load_img(path, target_size=(target_size, target_size))
+    test_image = image.load_img(path, target_size=(64, 64))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
     result = classifier.predict(test_image)
-    print(result)
+
     prediction = mapper(result, target)
     return prediction , path
-
-def voting_prediction(models, target, target_size, cropped=True, testAll = True):
-    wd = get_current_directory()
-    if cropped:
-        path1 = wd + '\\UTKFace_pred\\'#for cropped
-    else:
-        path1 = wd + '\part2\\'#for non-cropped
-
-    onlyfiles = [f for f in listdir(path1) if isfile(join(path1, f))]
-    len_ = len(onlyfiles)
-    if testAll == False:
-
-        random_pic = Path(random.choice(onlyfiles))
-        path = path1 / random_pic
-
-        test_image = image.load_img(path, target_size=(target_size, target_size))
-        test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis=0)
-
-        i=0
-        preds = []
-        for model in models:
-            result = model.predict(test_image)
-            preds.append(result[0])
-
-        pred = np.average(preds, axis=0)
-
-        if np.argmax(pred) == 0:
-            x='White'
-        elif np.argmax(pred) == 1:
-            x='Black'
-        elif np.argmax(pred) == 2:
-            x='Asian'
-        elif np.argmax(pred) == 3:
-            x='Indian'
-        else:
-            x='Other'
-
-        return x, path
-
-    else:
-        ground_truth = []
-        all_preds = []
-        for i in range(len_):
-            random_pic = Path(random.choice(onlyfiles))
-            path = path1 / random_pic
-            item = str(random_pic)
-            a = item.split("_", 3)
-            age, gender, ethnic, time = a[0], a[1], a[2], a[3]
-
-            if target=='age':
-                ground_truth.append(int(age))
-            elif target=='gender':
-                ground_truth.append(int(gender))
-            else:
-                ground_truth.append(int(ethnic))
-
-            test_image = image.load_img(path, target_size=(target_size, target_size))
-            test_image = image.img_to_array(test_image)
-            test_image = np.expand_dims(test_image, axis=0)
-
-            i = 0
-            preds = []
-            for model in models:
-                result = model.predict(test_image)
-                preds.append(result[0])
-            pred = np.average(preds, axis=0)
-            all_preds.append(np.argmax(pred))
-
-        if target == 'age':
-            print(np.square(np.subtract(np.array(ground_truth), np.array(all_preds))))
-            mse = np.mean(np.square(np.subtract(np.array(ground_truth), np.array(all_preds))))
-            print(mse)
-        else:
-            cm = confusion_matrix(ground_truth, all_preds)
-            print(classification_report(ground_truth, all_preds))
-            print(cm)
-
-
 
 def mapper(result, target):
     if target == 'ethnic':
@@ -358,69 +164,3 @@ def mapper(result, target):
         prediction = str(result[0][0])
 
     return prediction
-
-def data_generator_cust(df,im_width, im_height, for_training, path, batch_size):
-    images, ages, races, genders = [], [], [], []
-    n_races = len(df['ethnic'].unique())
-    df['age'] = df['age'].astype(float)
-    df['gender'] = df['gender'].astype(float)
-
-    max_age = 116
-
-
-    while True:
-        for i in range(len(df)):
-            r = df.iloc[i]
-            file, age, race, gender = Path(r['file_name']), r['age'], r['ethnic'], r['gender']
-            im = Image.open(path/file)
-            im = im.resize((im_width, im_height))
-            rgb_im = im.convert('RGB')
-            im = np.array(rgb_im) / 255.0
-            images.append(im)
-            ages.append(age / max_age)
-            races.append(to_categorical(race, n_races))
-            genders.append(gender)
-            if len(images) == batch_size:
-                yield np.array(images), [np.array(ages), np.array(races), np.array(genders)]
-                images, ages, races, genders = [], [], [], []
-        if not for_training:
-            break
-
-
-def make_new_p_multi(classifier):
-
-
-    path1 = project_dir / Path('UTKFace_pred')
-    onlyfiles = [f for f in listdir(path1) if isfile(join(path1, f))]
-    random_pic = Path(random.choice(onlyfiles))
-    path = path1 / random_pic
-
-    test_image = image.load_img(path, target_size=(224, 224))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis=0)
-    result = classifier.predict(test_image)
-    print(result)
-    prediction = mapper_result_multi(result)
-    return prediction, path
-
-def mapper_result_multi(result):
-    prediction = dict()
-
-    prediction['age'] = (int(result[0][0][0]*116))
-    gen = lambda x: 'Male' if x < .5 else 'Female'
-    prediction['gender'] = gen(result[2])
-
-    if np.argmax(result[1]) == 0:
-        x='White'
-    elif np.argmax(result[1]) == 1:
-        x='Black'
-    elif np.argmax(result[1]) == 2:
-        x='Asian'
-    elif np.argmax(result[1]) == 3:
-        x='Indian'
-    else:
-        x='Other'
-    prediction['Ethnicity'] = x
-
-    return str(prediction)
-
